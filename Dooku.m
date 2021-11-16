dc8544fa4919d7f9286349836a771c270c877d9b
PBre42 <49883776+PBre42@users.noreply.github.com>
2021-11-16 15:35:40 +0100

    Project setup

---
+++ Dooku.m
@@ -0,0 +1,7 @@
+%% Projet Vision : Résoudre un sudoku sans forcer !
+% Lelio Lardon - Lina Lahboub - Logan Cassier - Tom-Pierre Brégnac
+% L'utilisateur peut utiliser sa caméra ou ses fichiers pour résoudre des
+% grilles de sudoku. Il peut aussi créer de nouvelles grilles,
+% personnalisables.
+
+%%
